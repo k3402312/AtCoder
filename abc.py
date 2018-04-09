104f88c1f1e1328a656a660e4980838e8765267b
k3402312 <k3402312@kadai.jp>
2018-04-09 23:37:36 +0900

    ウンコード

---
+++ abc.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+s = input()
+
+if 'a' in s:
+    if 'b' in s:
+        if 'c' in s:
+            print("Yes")
+        else:
+            print("No")
+    else:
+        print("No")
+else:
+    print("No")
