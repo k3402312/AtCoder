2e3b9c9c126085afad921b2d92160bf6212e2b15
k3402312 <k3402312@kadai.jp>
2018-04-16 23:27:54 +0900

    Add fib

---
+++ fib_memo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define MAX_N 40
+
+int memo[MAX_N + 1];
+
+int fib(int n){
+    if (n <= 1) return n;
+    if (memo[n] != 0) return memo[n];
+    return memo[n] = fib(n - 1) + fib(n - 2);
+}
+
+int main(){
+    int n;
+    std::cin >> n;
+    if(n > MAX_N){
+        std::cout << "Please enter a number less than 40\n";
+        exit(0);
+    }
+    std::cout << fib(n) << std::endl;
+}
